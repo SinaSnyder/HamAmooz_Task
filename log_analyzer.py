7549fc46bcd3439e360d748202489dfae1c3c8b6
Sina <sinaaa733@gmail.com>
2026-07-09 16:50:59 +0330

    Add process_log_file to log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,6 +1,25 @@
 import os
 import argparse
 
+def process_log_file(file_path):
+    print("Reading...")
+    total_lines = 0
+    
+    with open(file_path , mode="r" , encoding="utf-8" , errors="ignore") as file:
+        for line in file :
+            
+            line = line.strip()
+            
+            if not line :
+                continue
+            
+            total_lines += 1
+
+            # if total_lines <= 5 :
+            #     print(f"Line {total_lines} : {line}")
+
+        print(f"Total Lines : {total_lines}")
+
 def main(): 
 
     parser = argparse.ArgumentParser()
@@ -14,6 +33,8 @@ def main():
         return
     
     print(f"{file_path} found, Start Process...")
+
+    process_log_file(file_path)
 
 if __name__ == "__main__" :
     main()    
